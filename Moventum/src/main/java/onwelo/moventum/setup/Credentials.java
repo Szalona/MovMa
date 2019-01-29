6c93f73bc9ca959f32acbd3c635964483ad56add
Szalona <44002942+Szalona@users.noreply.github.com>
2019-01-29 09:23:29 +0100

    Update Credentials.java

--- Moventum/src/main/java/onwelo/moventum/setup/Credentials.java
+++ Moventum/src/main/java/onwelo/moventum/setup/Credentials.java
@@ -2,7 +2,7 @@ package onwelo.moventum.setup;
 
 public class Credentials {
 
-    public static final String DELTA_USERNAME = "delta.afs.demo";
-    public static final String DELTA_PASSWORD = "movweb2014";
+    public static final String DELTA_USERNAME = "";
+    public static final String DELTA_PASSWORD = "";
 
 }
